856bb8739b88ddb95298e476f3270398794fadc1
zakuro <z@kuro.red>
2018-04-26 21:35:10 +0900

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,21 +5,25 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  env: {
+    ncmb: {
+      applicationKey: process.env.NCMB_APPLICATION_KEY,
+      clientKey: process.env.NCMB_APPLICATION_KEY,
+    },
   },
   build: {
     /* Run ESLint on save */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
     },
