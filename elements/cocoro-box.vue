9fa85c3b0f27aa1b808c92b0a083beebba8ebadb
z@kuro <z@kuro.red>
2018-04-29 15:49:58 +0900

    Update cocoro-box

--- elements/cocoro-box.vue
+++ elements/cocoro-box.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="box"
-    :style="styles">
+  <div :class="boxClass">
     <slot />
   </div>
 </template>
@@ -10,19 +8,42 @@
 export default {
   name: 'CocoroBox',
   props: {
-    height: {
-      type: Number,
-      default: 1,
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    secondary: {
+      type: Boolean,
+      default: false,
+    },
+    default: {
+      type: Boolean,
+      default: true,
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+    raised: {
+      type: Boolean,
+      default: false,
+    },
+    digged: {
+      type: Boolean,
+      default: false,
     },
   },
   computed: {
-    styles() {
+    boxClass() {
+      const color =
+        (this.primary ? 'primary' : this.secondary ? 'secondary' : 'default') +
+        (this.outline ? '-outline' : '')
+      const raise = this.raised ? 'raised-1' : 'raised-0'
       return {
-        'box-shadow': `0px ${this.shadowLen} 2px 35px rgba(0, 0, 0, 0.5)`,
+        box: true,
+        [`box--${color}`]: true,
+        [`box--${raise}`]: true,
       }
-    },
-    shadewLen() {
-      return (this.height || 0) * 3.0
     },
   },
 }
@@ -30,8 +51,31 @@ export default {
 
 <style scope>
 .box {
-  border-radius: 3px;
+  border-radius: 5px;
   padding: 5px;
-  box-shadow: 0px 3px 0px 0px rgba(0, 0, 0, 0.1);
+}
+
+.box--default {
+  background-color: #888;
+  color: #eee;
+}
+
+.box--primary {
+  background-color: #eaa;
+  color: #855;
+}
+
+.box--default-outline {
+  background-color: #eee;
+  color: #888;
+  border: solid 1px #888;
+}
+
+.box--raised-1 {
+  box-shadow: 0 3px 1px -1px rgba(0, 0, 0, 0.3);
+}
+
+.box--raised-0 {
+  box-shadow: 0 0;
 }
 </style>
--- elements/stories/index.js
+++ elements/stories/index.js
@@ -3,7 +3,24 @@ import { storiesOf } from '@storybook/vue'
 
 import CocoroBox from '../cocoro-box.vue'
 
-storiesOf('CocoroBox', module).add('simple', () => ({
-  components: { CocoroBox },
-  template: `<cocoro-box>Content</cocoro-box>`,
-}))
+storiesOf('CocoroBox', module)
+  .add('simple', () => ({
+    components: { CocoroBox },
+    template: `<cocoro-box>Content</cocoro-box>`,
+  }))
+  .add('outline', () => ({
+    components: { CocoroBox },
+    template: `<cocoro-box default outline>Content</cocoro-box>`,
+  }))
+  .add('raised', () => ({
+    components: { CocoroBox },
+    template: `<cocoro-box default raised>Content</cocoro-box>`,
+  }))
+  .add('raised outline', () => ({
+    components: { CocoroBox },
+    template: `<cocoro-box default outline raised>Content</cocoro-box>`,
+  }))
+  .add('primary raised', () => ({
+    components: { CocoroBox },
+    template: `<cocoro-box primary raised>Content</cocoro-box>`,
+  }))
