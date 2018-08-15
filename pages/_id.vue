7d53b8d3a6a61bd2f6659fc858e5ef943acdcbfb
z@kuro <z@kuro.red>
2018-08-15 21:54:05 +0900

    Add _id.vue

---
+++ pages/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <nuxt-child />
+</template>
+
+<script>
+export default {
+  async asyncData({ store, params }) {
+    await store.dispatch('records/fetchKind', params.id)
+  },
+}
+</script>
