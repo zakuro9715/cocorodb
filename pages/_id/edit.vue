a597737dcfc0ae3b49980cd6eccba82b2aac6d33
z@kuro <z@kuro.red>
2018-08-15 22:30:54 +0900

    Add update kind page

---
+++ pages/_id/edit.vue
@@ -0,0 +1,37 @@
+<i18n>
+ja:
+  title: データセット編集
+</i18n>
+
+<template>
+  <div>
+    <h1>{{ $t('title') }}</h1>
+    <kind-form
+      :kind="kind"
+      @submit="submit" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import KindForm from '~/components/kind-form.vue'
+
+export default {
+  components: { KindForm },
+  computed: {
+    ...mapState('records', ['kinds']),
+    kind() {
+      return this.kinds[this.$route.params.id]
+    },
+  },
+  methods: {
+    async submit(kind) {
+      await this.$store.dispatch('records/updateKind', kind)
+      this.$router.push('/')
+    },
+  },
+}
+</script>
+
+<style>
+</style>
