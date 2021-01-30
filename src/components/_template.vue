e399501fd3b6fa0f36b80007a62dd58a7469fce1
zakuro <z@kuro.red>
2021-01-30 23:12:21 +0900

    add _template.vue

---
+++ src/components/_template.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script lang="ts">
+import { } from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+  components: {
+  },
+})
+</script>
