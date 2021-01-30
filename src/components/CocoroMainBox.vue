88daf8eae5fb71fda1f14b1dfd1223fe84f3211e
zakuro <z@kuro.red>
2021-01-30 23:13:23 +0900

    Remove warns

--- src/components/CocoroMainBox.vue
+++ src/components/CocoroMainBox.vue
@@ -35,6 +35,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style scoped>
-</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -31,6 +31,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style scoped>
-</style>
