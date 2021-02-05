77684abb784417476415866d07003f053e50c38d
zakuro <z@kuro.red>
2021-02-05 16:03:40 +0900

    Remove unwrap

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,3 @@
-import { unwrap } from '@/utils'
 import { db, Item } from './db'
 
 export { Item } from './db'
@@ -14,8 +13,8 @@ export async function createMainItem(): Promise<number> {
   return await db.items.put({ id: MAIN_ITEM_ID, name: MAIN_ITEM_DEAFULT_NAME })
 }
 
-export async function getMainItem(): Promise<Item>  {
-  return await db.items.get({ id: MAIN_ITEM_ID }).then((v) => unwrap(v))
+export async function getMainItem(): Promise<Item | undefined>  {
+  return await db.items.get({ id: MAIN_ITEM_ID })
 }
 
 export async function prepare(): Promise<void> {
